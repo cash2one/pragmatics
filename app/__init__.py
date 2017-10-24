225ead2ed589911766a2a753baff231edd8ded12
harzo <harzojunior@gmail.com>
2017-10-24 19:58:42 +0200

    Move from sqlalchemy-migrations to Flask-migrations Recreating db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
+
+from app import models
+
+migrate = Migrate(app, db)
 
 from app import views
--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://newhope_pragma:pragma566I@s39.vdl.pl/newhope_pragma'
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'migrations')
+SQLALCHEMY_DATABASE_URI = 'postgresql://newhope_movies:pragma566I@s39.vdl.pl/newhope_movies'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
