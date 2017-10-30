84395b363fe7d5939fe43b38a525794f31649a46
harzo <wojciech.harzowski@atmat.pl>
2017-10-30 17:13:14 +0100

    Revoke usage of APScheduler, considering asyncio use for periodic task

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 #!flask/bin/python
 from ybsuggestions import app
-from ybsuggestions.crawler import scheduler
 
 if __name__ == "__main__":
-    #scheduler.start()
-
     app.run(host='0.0.0.0', debug=True)
--- ybsuggestions/crawler/__init__.py
+++
@@ -1,6 +0,0 @@
-from ybsuggestions import app
-from flask_apscheduler import APScheduler
-
-scheduler = APScheduler()
-scheduler.api_enabled = True
-scheduler.init_app(app)
